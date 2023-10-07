14d2811578c1d8e277857ebe274214e03bb0e9fe
Pascal Ballet <pascal.ballet@gmail.com>
2023-10-07 18:56:54 +0200

    Update IfMouseClick.gd

--- addons/BehavForGroups/IfMouseClick.gd
+++ addons/BehavForGroups/IfMouseClick.gd
@@ -16,20 +16,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func biodyn_process(agent)->bool:
-	if Input.is_mouse_button_pressed(btn):
-		if playerCamera == null:
-				playerCamera = get_tree().root.find_child("Camera3D", true, false)
-				if playerCamera == null:
-					print("Camera3D not found")
-					return false
-		var spaceState = agent.get_world_3d().direct_space_state
-		var mousePosition = agent.get_viewport().get_mouse_position()
-		var raycastOrigin = playerCamera.project_ray_origin(mousePosition)
-		var rayLength = 1000
-		var raycastTarget = raycastOrigin + playerCamera.project_ray_normal(mousePosition) * rayLength
-		var physicsRaycastQuery = PhysicsRayQueryParameters3D.create(raycastOrigin, raycastTarget)
-		var raycastResult = spaceState.intersect_ray(physicsRaycastQuery)
-		if raycastResult:
-			if raycastResult.collider == agent:
-				return true
-	return false			
+	if agent is CollisionObject3D:
+		if Input.is_mouse_button_pressed(btn):
+			if playerCamera == null:
+					playerCamera = get_tree().root.find_child("Camera3D", true, false)
+					if playerCamera == null:
+						print("Camera3D not found")
+						return false
+			var spaceState = agent.get_world_3d().direct_space_state
+			var mousePosition = agent.get_viewport().get_mouse_position()
+			var raycastOrigin = playerCamera.project_ray_origin(mousePosition)
+			var rayLength = 1000
+			var raycastTarget = raycastOrigin + playerCamera.project_ray_normal(mousePosition) * rayLength
+			var physicsRaycastQuery = PhysicsRayQueryParameters3D.create(raycastOrigin, raycastTarget)
+			var raycastResult = spaceState.intersect_ray(physicsRaycastQuery)
+			if raycastResult:
+				if raycastResult.collider == agent:
+					return true
+	
+	return false
