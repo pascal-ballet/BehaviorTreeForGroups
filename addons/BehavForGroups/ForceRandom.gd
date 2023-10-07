f45b89057afcdf52f71d58d7bd9c014c31ac5544
Pascal Ballet <pascal.ballet@gmail.com>
2023-10-07 17:28:23 +0200

    Adding 2D

--- addons/BehavForGroups/ForceRandom.gd
+++ addons/BehavForGroups/ForceRandom.gd
@@ -7,9 +7,19 @@ extends BehavForGroups
 @export var fz:float = 1.0
 
 func biodyn_process(agent)->bool:
-	var ffx:float = fx * (randf() - 0.5)
-	var ffy:float = fy * (randf() - 0.5)
-	var ffz:float = fz * (randf() - 0.5)
-	#call_deferred("agent.apply_central_force", Vector3 (ffx, ffy, ffz) )
-	agent.apply_central_impulse ( Vector3 (ffx, ffy, ffz) )
-	return true
+	if agent is RigidBody3D:
+		var ffx:float = fx * (randf() - 0.5)
+		var ffy:float = fy * (randf() - 0.5)
+		var ffz:float = fz * (randf() - 0.5)
+		#call_deferred("agent.apply_central_force", Vector3 (ffx, ffy, ffz) )
+		agent.apply_central_impulse ( Vector3 (ffx, ffy, ffz) )
+		return true
+	
+	if agent is RigidBody2D:
+		var ffx:float = fx * (randf() - 0.5)
+		var ffy:float = fy * (randf() - 0.5)
+		#call_deferred("agent.apply_central_force", Vector3 (ffx, ffy, ffz) )
+		agent.apply_central_impulse ( Vector2 (ffx, ffy) )
+		return true
+	
+	return false
