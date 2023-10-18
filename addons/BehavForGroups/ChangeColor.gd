67804e5d230d9e35b32853eea94cc5bc874627c2
Pascal Ballet <pascal.ballet@gmail.com>
2023-10-18 12:21:44 +0200

    added behavior : change color

---
+++ addons/BehavForGroups/ChangeColor.gd
@@ -0,0 +1,22 @@
+@icon("Action.svg")
+class_name ChangeColor
+extends BehavForGroups
+
+@export var color:Color = Color.CHARTREUSE
+
+func biodyn_process(agent)->bool:
+	if agent is RigidBody3D:
+		# New material
+		var mat:StandardMaterial3D = StandardMaterial3D.new()
+		mat.albedo_color = color
+		# Put the new material
+		var msh:MeshInstance3D     = agent.get_node("MeshInstance3D")
+		msh.material_override = mat
+		return true
+		
+	if agent is RigidBody2D:
+		var poly:Polygon2D = get_node("Polygon2D")
+		poly.Color = color
+		return true
+
+	return false
