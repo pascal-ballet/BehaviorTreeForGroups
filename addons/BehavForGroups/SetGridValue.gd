3073d6d388a8bd35974a2510aa1cb6be931e2746
Pascal Ballet <pascal.ballet@gmail.com>
2023-10-25 12:11:53 +0200

    Update SetGridValue.gd

--- addons/BehavForGroups/SetGridValue.gd
+++ addons/BehavForGroups/SetGridValue.gd
@@ -15,15 +15,16 @@ func biodyn_process(agent)->bool:
 	for grid in grids:
 		# 2D
 		if agent is Node2D:
-			var ax = agent.transform.origin.x
-			var ay = agent.transform.origin.y
-			var gx_min:float = grid.transform.origin.x
-			var gx_max:float = gx_min+grid.transform.scale.x
-			var gy_min:float = grid.transform.origin.y
-			var gy_max:float = gy_min+grid.transform.scale.y
+			var ax:float = agent.transform.origin.x
+			var ay:float = agent.transform.origin.y
+			var gx_min:float = grid.position.x
+			var gx_max:float = gx_min+grid.size.x
+			var gy_min:float = grid.position.y
+			var gy_max:float = gy_min+grid.size.y
 			if ax >= gx_min && ax < gx_max:
-				if ay >= gy_min && ay < gy_min:
-					var px:int = ((ax - grid.transform.origin.x) / grid.transform.origin.scale.x) * grid.SX
-					var py:int = ((ax - grid.transform.origin.y) / grid.transform.origin.scale.y) * grid.SY
-					grid[px+py*grid.SX] = set_value
+				if ay >= gy_min && ay < gy_max:
+					var px:int = ((ax - gx_min) / grid.size.x) * grid.SX
+					var py:int = ((ax - gy_min) / grid.size.y) * grid.SY
+					var p:int = px+py*grid.SX
+					grid.values_t0[p] = set_value
 	return true
