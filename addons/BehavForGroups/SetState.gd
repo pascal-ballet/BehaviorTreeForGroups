248174574cfb78aa8daa05d4e0528bc57bf38822
Pascal Ballet <pascal.ballet@gmail.com>
2023-11-07 10:52:09 +0100

    set_state corrected

--- addons/BehavForGroups/SetState.gd
+++ addons/BehavForGroups/SetState.gd
@@ -4,10 +4,11 @@ extends BehavForGroups
 
 ## New agent's state value
 @export var set_to_value:int = 0
-var update_state_at_age:int = 0
+var update_state_at_age:int = -1
 var agt:Node = null
 
 func biodyn_process(agent:Node)->bool:
+	try_update_state()
 	if agent.state != set_to_value:
 		update_state_at_age = agent.age+1
 		agt = agent
@@ -15,8 +16,11 @@ func biodyn_process(agent:Node)->bool:
 	return true
 
 func _process(_delta):
-	if update_state_at_age != 0:
-		if update_state_at_age == agt.age:
-			agt.state = set_to_value
-			update_state_at_age = 0
+	try_update_state()
 
+func try_update_state():
+	if update_state_at_age != -1:
+		if is_instance_valid(agt):
+			if update_state_at_age <= agt.age:
+				agt.state = set_to_value
+				update_state_at_age = -1
