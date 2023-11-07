eb7a92b65654fa47343b53e6aa9b13d790c552ff
Pascal Ballet <pascal.ballet@gmail.com>
2023-11-07 14:38:43 +0100

    Coagulation updated

--- addons/BehavForGroups/IfStateEquals.gd
+++ addons/BehavForGroups/IfStateEquals.gd
@@ -2,10 +2,11 @@
 class_name IfStateEquals
 extends BehavForGroups
 
+## Test the state value (-1 for any state)
 @export var state_equals:int = 0
 
 func biodyn_process(agent:Node)->bool:
-	if agent.state == state_equals:
+	if agent.state == state_equals || state_equals == -1:
 		return true
 	else:
 		return false
