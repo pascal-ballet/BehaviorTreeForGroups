8185382e4dfd89d03c6727ab18c112094f2d73aa
Pascal Ballet <pascal.ballet@gmail.com>
2024-11-16 20:37:59 +0100

    added names to reactives and products in Reaction and Grid2d

--- addons/behav_tree_for_groups/field.gd
+++ addons/behav_tree_for_groups/field.gd
@@ -4,5 +4,6 @@ class_name Field
 
 @export var name:String = "Field"
 @export var color:Color = Color.BLUE
-@export var diffusion_speed:float = 0.1
-@export var degradation_speed:float = 0.01
+@export_range(0.0, 1.0) var diffusion_speed:float = 0.3
+@export var degradation_speed:float = 0.001
+@export_enum("0", "1", "Random") var initialisation: String = "0"
